259c1e8003a63da25f02fe432055cc5b758d57ff
Shray Thakur <ProgrammerShray6@gmail.com>
2025-11-27 19:28:49 +0530

    created new files and all the screens

--- app/src/main/java/com/jps/apps/jiva_nshaastra/HomeActivity.java
+++ app/src/main/java/com/jps/apps/jiva_nshaastra/HomeActivity.java
@@ -3,12 +3,17 @@ package com.jps.apps.jiva_nshaastra;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        name = findViewById(R.id.user);
     }
 }
---
+++ app/src/main/java/com/jps/apps/jiva_nshaastra/philosphies/Sph.java
@@ -0,0 +1,16 @@
+package com.jps.apps.jiva_nshaastra.philosphies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jps.apps.jiva_nshaastra.R;
+
+public class Sph extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sph);
+    }
+}
