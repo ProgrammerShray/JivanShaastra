f4b25a14aabf38489b11b1e1c30969ef6a728d7d
Shray Thakur <ProgrammerShray6@gmail.com>
2025-12-19 20:59:52 +0530

    changes in the login frontend and backend

--- app/src/main/java/com/jps/apps/jiva_nshaastra/MainActivity.java
+++ app/src/main/java/com/jps/apps/jiva_nshaastra/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private void signUp(String name, String email, String password, String dob){
-        String url = "http://127.0.0.1:5000/auth/signup";
+        String url = "http://192.168.178.159:5000/auth/signup";
 
         RequestQueue queue = Volley.newRequestQueue(this);
 
