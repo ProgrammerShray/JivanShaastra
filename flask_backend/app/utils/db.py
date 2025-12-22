dae2987642bc447c69662a4da30a316fcea998b0
Shray Thakur <ProgrammerShray6@gmail.com>
2025-12-22 08:21:48 +0530

    Updated db.py

--- flask_backend/app/utils/db.py
+++ flask_backend/app/utils/db.py
@@ -1,15 +1,15 @@
-import mysql.connector
+import psycopg2
 from flask import g, current_app
+
 
 def get_db():
     if "db" not in g:
-        g.db = mysql.connector.connect(
-            host=current_app.config["MYSQL_HOST"],
-            user=current_app.config["MYSQL_USER"],
-            password=current_app.config["MYSQL_PASSWORD"],
-            database=current_app.config["MYSQL_DB"]
+        g.db = psycopg2.connect(
+            current_app.config["DATABASE_URL"],
+            sslmode="require"  # IMPORTANT for Neon
         )
     return g.db
+
 
 def close_db(e=None):
     db = g.pop("db", None)
