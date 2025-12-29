be2b02edf4e7076a41a900b2fe8a0934e20a9b2f
Shray Thakur <ProgrammerShray6@gmail.com>
2025-12-29 21:25:29 +0530

    Update config.py

--- flask_backend/config.py
+++ flask_backend/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     DATABASE_URL = os.getenv("DATABASE_URL")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("SECRET_KEY")
 
     if DATABASE_URL and DATABASE_URL.startswith("psql://"):
         DATABASE_URL = DATABASE_URL.replace("psql://", "postgresql://", 1)
