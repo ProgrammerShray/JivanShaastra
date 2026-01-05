ef644f6c2d7f577d802752ec62b05e8b25ce509b
Shray Thakur <ProgrammerShray6@gmail.com>
2026-01-05 16:51:07 +0530

    added sav files

---
+++ app/src/main/java/com/jps/apps/jiva_nshaastra/philosphies/ShloakLists/SavShlokList.java
@@ -0,0 +1,14 @@
+package com.jps.apps.jiva_nshaastra.philosphies.ShloakLists;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SavShlokList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sav_shlok_list);
+    }
+}
