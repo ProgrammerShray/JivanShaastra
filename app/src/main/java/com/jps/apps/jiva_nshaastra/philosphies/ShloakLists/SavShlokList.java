1125f09add4f9c3a75d9872581e617fb35ea00e4
Shray Thakur <ProgrammerShray6@gmail.com>
2026-01-06 12:57:21 +0530

    SAV development started

--- app/src/main/java/com/jps/apps/jiva_nshaastra/philosphies/ShloakLists/SavShlokList.java
+++ app/src/main/java/com/jps/apps/jiva_nshaastra/philosphies/ShloakLists/SavShlokList.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.jps.apps.jiva_nshaastra.R;
+
 public class SavShlokList extends AppCompatActivity {
 
     @Override
